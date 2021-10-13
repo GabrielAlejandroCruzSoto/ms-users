85acbb1efb49c1d786548a81d4c957fd58cc3d64
Gabriel Cruz Soto <gabrielcrsoto@outlook.com>
2021-10-13 19:27:58 -0300

    Project initial

---
+++ src/main/java/com/pgn/ms/users/PgnMsUsersApplication.java
@@ -0,0 +1,13 @@
+package com.pgn.ms.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgnMsUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PgnMsUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pgn/ms/users/PgnMsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgn.ms.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgnMsUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
